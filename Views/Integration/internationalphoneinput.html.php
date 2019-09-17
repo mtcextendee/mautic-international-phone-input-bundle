b314ca251c902c7c1c2691e13db6a34716ae68ee
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-09-17 16:56:11 +0200

    in builder without scripts

--- Views/Integration/internationalphoneinput.html.php
+++ Views/Integration/internationalphoneinput.html.php
@@ -12,7 +12,7 @@ $containerType     = (isset($type)) ? $type : 'text';
 $defaultInputClass = (isset($inputClass)) ? $inputClass : 'input';
 include __DIR__.'/../../../../app/bundles/FormBundle/Views/Field/field_helper.php';
 
-$formName= !empty($formName) ? $formName : 'preview';
+$formName= !empty($formName) ? $formName : '';
 
 $input = $view->render(
     'MauticFormBundle:Field:text.html.php',
@@ -21,23 +21,32 @@ $input = $view->render(
         'inForm'     => (isset($inForm)) ? $inForm : false,
         'type'       => 'text',
         'id'         => $id,
-        'formId'     => (isset($formId)) ? $formId : 0,
+        'formId'     => (isset($formId)) ? $formId : 'preview',
         'formName'   => (isset($formName)) ? $formName : '',
         'inputClass' => 'input inttel'.$formName,
     ]
 );
 echo str_replace('<input', '<input data-field-alias="'.$field['alias'].'"', $input);
+    $elementId = 'mauticform_input'.$formName.'_'.$field['alias'];
 
-$elementId = 'mauticform_input'.$formName.'_'.$field['alias'];
-
-echo <<<HTML
+if(!$inBuilder) {
+    echo <<<HTML
 <link rel="stylesheet" href="{$view['assets']->getUrl(
-    'plugins/MauticInternationalPhoneInputBundle/Assets/css/intlTelInput.min.css', null, null, true
-)}">
-<script src="{$view['assets']->getUrl('plugins/MauticInternationalPhoneInputBundle/Assets/js/intlTelInput.min.js', null, null, true)}"></script>
+        'plugins/MauticInternationalPhoneInputBundle/Assets/css/intlTelInput.min.css',
+        null,
+        null,
+        true
+    )}">
+<script src="{$view['assets']->getUrl(
+        'plugins/MauticInternationalPhoneInputBundle/Assets/js/intlTelInput.min.js',
+        null,
+        null,
+        true
+    )}"></script>
 <script async defer src="{$view['router']->generate(
-    'mautic_country_code_generate',
-    ['formName' => $formName],
-    true
-)}"></script>
+        'mautic_country_code_generate',
+        ['formName' => $formName],
+        true
+    )}"></script>
 HTML;
+}
