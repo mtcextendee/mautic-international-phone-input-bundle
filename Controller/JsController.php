3c5cf64d2f5ec06323f8f3090c8167f2370e8b75
enguerr <enguer@enguer.com>
2020-05-12 09:22:19 +0200

    force international value phone field

--- Controller/JsController.php
+++ Controller/JsController.php
@@ -33,12 +33,12 @@ class JsController extends CommonController
      * PublicController constructor.
      *
      * @param IpLookupHelper $ipLookupHelper
-     * @param AssetsHelper   $assetsHelper
+     * @param AssetsHelper $assetsHelper
      */
     public function __construct(IpLookupHelper $ipLookupHelper, AssetsHelper $assetsHelper)
     {
         $this->ipLookupHelper = $ipLookupHelper;
-        $this->assetsHelper   = $assetsHelper;
+        $this->assetsHelper = $assetsHelper;
     }
 
     /**
@@ -48,16 +48,17 @@ class JsController extends CommonController
      */
     public function generateCountryCodeAction($formName)
     {
-        $ip       = $this->ipLookupHelper->getIpAddress();
-        include_once __DIR__.'/../countries.php';
-        $country  = ArrayHelper::getValue('country', $ip->getIpDetails());
+        $ip = $this->ipLookupHelper->getIpAddress();
+        include_once __DIR__ . '/../countries.php';
+        $country = ArrayHelper::getValue('country', $ip->getIpDetails());
         $countryCode = array_search(strtolower($country), array_map('strtolower', $countries)); ## easy version
         $realFormName = ltrim($formName, '_');
         $utilsUrl = $this->assetsHelper->getUrl('plugins/MauticInternationalPhoneInputBundle/Assets/js/utils.js', null, null, true
         );
-        $js       = <<<JS
+        $js = <<<JS
         if(!window.{$formName}){
        var elems = document.getElementsByClassName('inttel{$formName}');
+       var oldvalue = '';
        
        for(var i=0;i< elems.length;i ++) {
                var elem = elems[i];
@@ -66,6 +67,7 @@ class JsController extends CommonController
             hiddenInput: elem.getAttribute('data-field-alias')+'_full',
             separateDialCode: true,
           initialCountry: "{$countryCode}",
+          preferredCountries: [ "fr", "us", "ca" ],
           utilsScript: "{$utilsUrl}"
      });
       window.{$formName} = true;
@@ -78,13 +80,36 @@ class JsController extends CommonController
   
   
 MauticFormCallback['{$realFormName}'] = {
+    onValidateEnd: function (formValid) {
+        if (formValid){
+            // before form submit
+            for(var i=0;i< elems.length;i ++) {
+                var elem = elems[i];
+                var iti  = window.intlTelInputGlobals.getInstance(elem);
+                oldvalue = elem.value;
+                elem.value = iti.getNumber();
+            }
+        }
+    },
+    onResponseEnd: function (response) {
+         // called to process the response to the form submission
+         if (!response.success){
+            for(var i=0;i< elems.length;i ++) {
+                var elem = elems[i];
+                elem.value = oldvalue;
+            }             
+         }
+    },
     onValidateStart: function () {
-         for(var i=0;i< elems.length;i ++) {
-    var elem = elems[i];
-    var iti  = window.intlTelInputGlobals.getInstance(elem);
-      elem.nextSibling.value = iti.getNumber();
-      }
-}
+        for(var i=0;i< elems.length;i ++) {
+            var elem = elems[i];
+            var iti  = window.intlTelInputGlobals.getInstance(elem);
+            elem.nextSibling.value = iti.getNumber();
+        }
+    },
+    onErrorMark: function (callbackData) {
+         // called during error marking
+    }
 };
     
 
